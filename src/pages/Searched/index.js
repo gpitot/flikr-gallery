5c3754f2a413dbde32641d3fa84834cc9c3c05ec
guillaume <gpitot@nine.com.au>
2020-01-28 21:30:02 +1100

    searched page

--- src/pages/Searched/index.js
+++ src/pages/Searched/index.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
-
-
+import SearchBar from "../../components/SearchBar";
+import Results from "../../components/Results";
+import Demos from "../../components/Demos";
 //get query from params
 
 class Searched extends Component {
-    
-    
+  render() {
+    return (
+      <>
+        <SearchBar />
+        <Demos />
+        <Results query={this.props.match.params.query || null}/>
+      </>
+    );
+  }
 }
+
+export default Searched;
