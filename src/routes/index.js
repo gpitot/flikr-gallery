6670b3dd86c2ed5df822f94d125dff33e077a904
guillaume <gpitot@nine.com.au>
2020-01-28 21:30:16 +1100

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,20 @@
-import React, {Component} from "react";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
-
-
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Searched from "../pages/Searched";
+import PageNotFound from "../pages/PageNotFound";
 class Routes extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Searched} />
+          <Route path="/search/:query" component={Searched} />
 
-
-    render() {
-        <BrowserRouter>
-            <Switch>
-                <Route path="/search/:query" component = {Searched} />
-
-                <Route component={PageNotFound} />
-            </Switch>
-        </BrowserRouter>
-    }
+          <Route component={PageNotFound} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default Routes;
