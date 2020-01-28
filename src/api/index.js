ad808e798af52f083728fc962e2bc94166c4d083
guillaume <gpitot@nine.com.au>
2020-01-28 21:29:07 +1100

    api functions

---
+++ src/api/index.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import config from "./config";
+
+const { FLIKR_SECRET } = config;
+const BASE =
+  "https://www.flickr.com/services/rest/?method=flickr.photos.search";
+
+//request photos from flikr
+const loadPhotos = ({ query, limit = 20 }) => {
+  return new Promise((resolve, reject) => {
+    const url = `${BASE}&api_key=${FLIKR_SECRET}&tags=${query}&per_page=${limit}&format=json&nojsoncallback=1`;
+    console.log(url);
+    axios
+      .get(url)
+      .then(response => {
+        console.log(response);
+        return resolve(response.data.photos.photo);
+      })
+      .catch(err => reject([]));
+  });
+};
+
+export { loadPhotos };
